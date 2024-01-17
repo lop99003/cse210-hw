a57666cd6d328e503821e8ae5b8c2eb66e87e564
lop99003 <lop99003@byui.edu>
2024-01-17 15:40:30 -0600

    Commit and Push W02 Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,65 @@
-using System;
+// Job.cs
+public class Job
+{
+    // Member variables
+    public string _company; // Stores the company name
+    public string _jobTitle; // Stores the job title
+    public int _startYear; // Stores the start year of the job
+    public int _endYear; // Stores the end year of the job
 
+    // Display method
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+// Resume.cs
+public class Resume
+{
+    // Member variables
+    public string _personName; // Stores the person's name
+    public List<Job> _jobs = new List<Job>(); // Stores a list of Job instances
+
+    // Add job to the list
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    // Display method
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine("Jobs:");
+        foreach (var job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
+// Program.cs
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create Job instances
+        Job job1 = new Job { _company = "Microsoft", _jobTitle = "Software Engineer", _startYear = 2019, _endYear = 2022 };
+        Job job2 = new Job { _company = "Apple", _jobTitle = "Manager", _startYear = 2022, _endYear = 2023 };
+
+        // Test Job class
+        job1.Display();
+        job2.Display();
+
+        // Create Resume instance
+        Resume myResume = new Resume { _personName = "Allison Rose" };
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Test Resume class
+        myResume.Display();
     }
 }
